48fbcf2c176288cf4f71d2c5ee3171d70e562554
arvid <arvid@arvidpm.com>
2016-02-29 14:07:51 +0100

    added src for lab 4

---
+++ lab4/sigHand.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+static void sig_handler(int signo) {
+    if (signo == SIGINT) {
+        printf("received SIGINT\n");
+    }
+}
+
+static void another_handler(int sig, siginfo_t *si, void *context) {
+    printf("received signal %d\n", sig);
+}
+
+int main(void) {
+    struct sigaction sa;
+
+
+
+    if (signal(SIGINT, sig_handler) == SIG_ERR) { // subscribe sig_handler to SIGINT
+        printf("\ncan't catch SIGINT\n");
+    }
+
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = &another_handler;
+    if (sigaction(SIGHUP, &sa, NULL) == -1) { // same thing in more complicated for SIGHUP
+        printf("\ncan't catch SIGHUP\n");
+    }
+    
+    printf("Send some signal to process %d\n", getpid());
+
+    // A long long wait so that we can easily issue a signal to this process
+    while (1) {
+        sleep(1);
+    }
+    return 0;
+}
