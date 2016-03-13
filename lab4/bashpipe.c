4371464a2675c4a37619e1f11d1fc87e3d5bfa04
arvidpm <arvid@arvidpm.com>
2016-03-13 20:28:18 +0100

    merged everything into main

--- lab4/bashpipe.c
+++ lab4/bashpipe.c
@@ -7,90 +7,12 @@ amoosavi@kth.se
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-
-void child1(int *fd) {
-
-    /* First child preparing first part of the pipe */
-    printf("\nChild 1: preparing 'ls -la'\n");
-    printf("------------------------------\n");
-
-
-    pid_t pid1 = fork();
-
-    if (pid1 == -1) {
-        perror("fork failed");
-        exit(EXIT_FAILURE);
-    }
-   
-    else if (pid1 == 0) {
-
-        /* Closing read end of the pipe. */
-        close(fd[0]);
-
-        /* Duplicate a file descriptor.
-        The dup2() system call performs the same task as dup(), 
-        but instead of using the lowest-numbered unused file descriptor, 
-        it uses the descriptor number specified in newfd (=STDOUT). */
-        dup2(fd[1],STDOUT_FILENO);
-
-        /* Closing write end of the pipe. */
-        close(fd[1]);
-
-        /* int execlp(const char *file, const char *arg, ...);
-        The  execlp(), execvp(), and execvpe() functions duplicate 
-        the actions of the shell in searching for an executable file 
-        if the specified filename does not contain a '(/)' character.*/
-        execlp("ls", "ls", "-la", NULL);
-
-        _exit(EXIT_SUCCESS);
-    }
-
-    
-}
-
-void child2(int *fd) {
-
-    /* First child preparing first part of the pipe */
-    printf("\nChild 2: preparing 'less'\n");
-    printf("------------------------------\n");
-
-
-    pid_t pid2 = fork();
-
-    if (pid2 == -1) {
-        perror("fork failed");
-        exit(EXIT_FAILURE);
-    }
-   
-    else if (pid2 == 0) {
-
-        /* Closing write end of the pipe. */
-        close(fd[1]);
-
-        /* Duplicate a file descriptor.
-        The dup2() system call performs the same task as dup(), 
-        but instead of using the lowest-numbered unused file descriptor, 
-        it uses the descriptor number specified in newfd (=STDIN). */
-        dup2(fd[0], STDIN_FILENO);
-
-        /* Closing read end of the pipe. */
-        close(fd[0]);
-
-        /* int execlp(const char *file, const char *arg, ...);
-        The  execlp(), execvp(), and execvpe() functions duplicate 
-        the actions of the shell in searching for an executable file 
-        if the specified filename does not contain a '(/)' character.*/
-        execlp("less", "less", NULL);
-        //execl("/bin/less", "less", NULL);
-
-        printf("\nFinished 'ls -la | less'\n");
-        _exit(EXIT_SUCCESS);
-    }
-}
 
 int main(int argc, char **argv) {
-    
+
+    /* Creating a pipe array */
     int fd[2];
     
     /* pipe()  creates a pipe, a unidirectional data channel that can be 
@@ -101,12 +23,91 @@ int main(int argc, char **argv) {
     fd[1] refers to the write end of the pipe.*/
     pipe(fd);
 
+    /* Declarations */
+    pid_t pid1, pid2;
+
+    /* Forking first child */
+    pid1 = fork();
+
+    if (pid1 == -1) {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    }
+   
+    else if (pid1 == 0) {
+
+        /* First child preparing first part of the pipe */
+        printf("\nChild 1: preparing 'ls -la'\n");
+        printf("------------------------------\n");
+
+        /* Duplicate a file descriptor.
+        The dup2() system call performs the same task as dup(), 
+        but instead of using the lowest-numbered unused file descriptor, 
+        it uses the descriptor number specified in newfd (=STDOUT). */
+        dup2(fd[1], 1);
+
+        /* Closing write end of the pipe. */
+        close(fd[1]);
+        /* Closing read end of the pipe. */
+        close(fd[0]);
+
+        /* int execlp(const char *file, const char *arg, ...);
+        The  execlp(), execvp(), and execvpe() functions duplicate 
+        the actions of the shell in searching for an executable file 
+        if the specified filename does not contain a '(/)' character.*/
+        execlp("ls", "ls", "-la", NULL);
+
+        _exit(EXIT_SUCCESS);
+    }
+
+    else {
+
+        /* Forking second child */
+        pid2 = fork();
+
+        if (pid2 == -1) {
+            perror("fork failed");
+            exit(EXIT_FAILURE);
+        }
+    
+        else if (pid2 == 0) {
+
+            /* Second child preparing first part of the pipe */
+            printf("\nChild 2: preparing 'less'\n");
+            printf("------------------------------\n");
+
+            /* Duplicate a file descriptor.
+            The dup2() system call performs the same task as dup(), 
+            but instead of using the lowest-numbered unused file descriptor, 
+            it uses the descriptor number specified in newfd (=STDIN). */
+            dup2(fd[0], 0);
+
+            /* Closing write end of the pipe. */
+            close(fd[1]);
+            /* Closing read end of the pipe. */
+            close(fd[0]);
+            
+            /* int execlp(const char *file, const char *arg, ...);
+            The  execlp(), execvp(), and execvpe() functions duplicate 
+            the actions of the shell in searching for an executable file 
+            if the specified filename does not contain a '(/)' character.*/
+            execlp("less", "less", NULL);
+
+            _exit(EXIT_SUCCESS);
+
+        }
+
+    }
+
     /* Closing both ends for parent since children are doing the work*/
     close(fd[0]);
     close(fd[1]);
 
-    child1(fd);
-    child2(fd);
+    /* Waiting for children to complete */
+    wait(&pid1);
+    wait(&pid2);
 
-    exit(0);
+
+    return 0;
+    
 }
